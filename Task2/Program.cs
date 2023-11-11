2cc200c19dacb440e74f9f4f0ad78ee2246e5a3f
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-11 15:50:52 +0300

    Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,26 @@
+﻿double[] EnterNumbers() //метод ввода чисел через , 
+{
+    Console.Write("Введите через , b1,k1,b2,k2 ");
+    string input = Console.ReadLine();
+    string[] arrayNumbers = input.Split(","); // .Split метод разделения текста (символ которым разделяют | , # и тд .)
+    double[] numbers = new double[arrayNumbers.Length];
+    for(int i  = 0; i < arrayNumbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(arrayNumbers[i]);
+    }
+    return numbers;
+}
+
+void LinearEquation(double [] array)
+{
+//x=(b2-b1)/(k1-k2) , y=k1(b2-b1)/(k1-k2)+b1 
+double b1 = array[0]; // 0
+double k1 = array[1]; // 1 
+double b2 = array[2]; // 2
+double k2 = array[3]; // 3 
+double x = (b2-b1)/(k1-k2);
+double y = k1*(b2-b1)/(k1-k2)+b1;
+Console.WriteLine($"Прямые пересекаются в точке x = {x} y = {y}");
+}
+double[] num = EnterNumbers();
+LinearEquation(num);
