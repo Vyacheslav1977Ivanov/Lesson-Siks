9478f998e15a45f9e6b7cb7fc50fcb21ddcfb567
Vyacheslav Ivanov <vya1977@mail.ru>
2023-11-11 15:48:09 +0300

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,28 @@
+﻿double[] EnterNumbers() //метод ввода чисел через , 
+{
+    Console.Write("Введите числа через , ");
+    string input = Console.ReadLine();
+    string[] arrayNumbers = input.Split(","); // .Split метод разделения текста (символ которым разделяют | , # и тд .)
+    double[] numbers = new double[arrayNumbers.Length];
+    for(int i  = 0; i < arrayNumbers.Length; i++)
+    {
+        numbers[i] = Convert.ToDouble(arrayNumbers[i]);
+    }
+    return numbers;
+}
+
+void NegativeNumbers(double[] array)
+{
+    double result = 0;
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i] < 0)
+        {
+         result++;
+        }
+    }
+    Console.Write($"Кол-во отрицательных чисел = {result}");
+}
+
+double[] arr = EnterNumbers();
+NegativeNumbers(arr);
